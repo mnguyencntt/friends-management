dd1ed68db6eff344d7d732e6ebecd9dd621af99d
Minh Nguyen <m.nguyencntt@gmail.com>
2018-08-25 22:50:33 +0800

    add swagger ui for restful info

--- src/main/java/com/moon/app/MvcConfig.java
+++ src/main/java/com/moon/app/MvcConfig.java
@@ -6,11 +6,14 @@ package com.moon.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("com.moon.app")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
@@ -25,6 +28,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     resolver.setPrefix("/WEB-INF/jsp/");
     resolver.setSuffix(".jsp");
     return resolver;
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
 }
--- src/main/java/com/moon/app/WebSecurityConfig.java
+++ src/main/java/com/moon/app/WebSecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
