37c5d222c4b263edef7d380ac1c421f32880b441
Minh Nguyen <m.nguyencntt@gmail.com>
2018-08-25 22:17:07 +0800

    add init list emails

--- src/main/java/com/moon/app/Application.java
+++ src/main/java/com/moon/app/Application.java
@@ -1,16 +1,28 @@
 package com.moon.app;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.moon.app.exception.TechnicalException;
+import com.moon.app.service.AccountService;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
+
+  @Autowired
+  private AccountService accountService;
 
   static {
     // TODO Set proxy here.
@@ -26,6 +38,18 @@ public class Application extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(Application.class);
+  }
+
+  @PostConstruct
+  public void init() {
+    final List<String> initEmails = Arrays.asList("andy@example.com", "john@example.com", "lisa@example.com", "minh@example.com");
+    initEmails.forEach(email -> {
+      try {
+        accountService.createByEmail(email);
+      } catch (TechnicalException e) {
+        e.printStackTrace();
+      }
+    });
   }
 
 }
